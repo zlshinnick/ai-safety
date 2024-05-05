61f9f7ff1360c8fc98d8cda8671c3c786d5f31f5
Max <maxjtwelftree@gmail.com>
2024-05-05 17:50:57 +0930

    uploading

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup, find_packages
 
+from setuptools import setup, find_packages
+
 setup(
-    name="ai_safety",
-    version="0.1",
+    name='ai-safety',
+    version='0.1',
     packages=find_packages(),
+    description='Open source screening software enhancing safety in large language models',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Zach Shinnick',
+    author_email='Zachshinnick03@outlook.com',
+    url='https://github.com/zlshinnick/ai-safety.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
